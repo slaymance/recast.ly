4a9d3220cdad1dc9d599cb91c9668b57c5b5d3a4
Shane Laymance <shane.laymance@gmail.com>
2017-09-09 11:37:37 -0700

    Throttled live search working

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,19 +1,42 @@
-var Search = (props) => {
-  let handleSubmit = function(e) {
-    console.log(e);
-    let options = {query: $('.form-control').val(), max: 5};
-    searchYouTube(options, props.searchCallback);
-  };
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {input: ''};
+    this.handleSubmit = _.throttle(this.handleSubmit, 500);
+  }
 
-  return (
-    <div className="search-bar form-inline">
-      <input className="form-control" type="text" />
-      <button className="btn hidden-sm-down" onClick={handleSubmit}>
-        <span className="glyphicon glyphicon-search"></span>
-      </button>
-    </div>);
-};
+  handleSubmit () {
+    let options = {query: this.state.input, max: 5};
+    searchYouTube(options, this.props.searchCallback);
+  }
+
+  handleLiveSearch(event) {
+    this.setState({input: event.target.value});
+  }
+
+  render() {
+    return (
+      <div className="search-bar form-inline">
+        <input className="form-control" onChange={event => {
+          this.handleLiveSearch(event);
+          this.handleSubmit();
+        }} type="text" />
+        <button className="btn hidden-sm-down" onClick={this.handleSubmit.bind(this)}>
+          <span className="glyphicon glyphicon-search"></span>
+        </button>
+      </div>
+    );
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.Search = Search;
+
+/*
+
+
+
+
+
+*/
