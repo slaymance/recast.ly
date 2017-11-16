22b57b2cac35f8ccba8e5b9178106f65bfe94e58
Shane Laymance <shane.laymance@gmail.com>
2017-11-16 14:40:09 -0800

    Resolve merge conflicts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {video: props.videos[0], videos: this.props.videos};
-  }
-
-  callback(videoSelected) {
-    this.setState({video: videoSelected});
-  }
-
-  searchCallback(newVideos) {
-    this.setState({videos: newVideos});
-  }
-
-  render() {
-    return (
-      <div>
-        <nav className="navbar">
-          <div className="col-md-6 offset-md-3">
-            <Search searchCallback={this.searchCallback.bind(this)}/>
-          </div>
-        </nav>
-        <div className="row">
-          <div className="col-md-7">
-            <VideoPlayer video={this.state.video}/>
-          </div>
-          <div className="col-md-5">
-            <VideoList videos={this.state.videos} callback={this.callback.bind(this)}/>
-          </div>
-        </div>
-=======
 import React from 'react';
 
 const App = () => (
@@ -45,11 +13,10 @@ const App = () => (
       </div>
       <div className="col-md-5">
         <div><h5><em>videoList</em> view goes here</h5></div>
->>>>>>> 998583ed8bf7034458763ec703ea9f2eae00ecf2
       </div>
-    );
-  }
-}
+    </div>
+  </div>
+);
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
