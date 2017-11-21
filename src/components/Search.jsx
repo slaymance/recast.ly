7a061fd9454b59fcc4761f5792ca7dc68fc2ec1c
Shane Laymance <shane.laymance@gmail.com>
2017-11-21 00:32:23 -0800

    Implement AJAX YouTube search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
+import searchYouTube from '../lib/searchYouTube';
 
-const Search = () => (
-  <div className="search-bar form-inline">
-    <input className="form-control" type="text" />
-    <button className="btn hidden-sm-down">
-      <span className="glyphicon glyphicon-search" />
-    </button>
-  </div>
-);
+const Search = () => {
+  let input;
+  return (
+    <div className="search-bar form-inline">
+      <input
+        className="form-control"
+        type="text"
+        ref={(node) => {
+          input = node;
+        }}
+      />
+      <button
+        className="btn hidden-sm-down"
+        onClick={() => {
+          searchYouTube({
+            query: input.value,
+            max: 5
+          })
+          input.value = '';
+        }}
+      >
+        <span className="glyphicon glyphicon-search" />
+      </button>
+    </div>
+  );
+};
 
 export default Search;
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,28 @@
-var searchYouTube = (options, callback) => {
-  // TODO
+import $ from 'jquery';
+import updateVideoList from '../redux/actions';
+import YOUTUBE_API_KEY from '../config/youtube';
+
+const searchYouTube = (options) => {
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    data: {
+      maxResults: options.max,
+      part: 'snippet',
+      q: options.query,
+      type: 'video',
+      key: YOUTUBE_API_KEY,
+    },
+    contentType: 'application/json',
+    dataType: 'json',
+    success: (data) => {
+      console.log('AJAX request successful');
+      console.log(data.items);
+      updateVideoList(data.items);
+    },
+    error: () => {
+      console.error('AJAX request failed');
+    },
+  });
 };
 
-window.searchYouTube = searchYouTube;
+export default searchYouTube;
